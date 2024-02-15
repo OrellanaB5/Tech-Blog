12ea093b11ac6362bec97bce46fd402ef0a6e111
Brandon <brandondenzell566@outlook.com>
2024-02-14 23:26:40 -0500

    pineapple

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,25 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) { 
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false 
+            }
+        }
+    });
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    }
-  );
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: 3306,
+    });
 }
 
 module.exports = sequelize;
+
