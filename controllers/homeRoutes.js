c0cba59bf355dba28a54b0e91d08019711a61fc9
Brandon <brandondenzell566@outlook.com>
2024-02-14 21:56:48 -0500

    work

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [User],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("home", {
+      posts,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+    if (postData) {
+      const post = postData.get({ plain: true });
+      res.render("onePost", {
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+module.exports = router;
